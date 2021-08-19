aaac881e7bc1f4b942d6fe5974e14ca25999e31f
khaliq Dad <khaliq.bestweb@gmail.com>
2021-08-19 17:40:16 +0800

    added library to main app

--- app/src/main/java/com/emericoapp/demolibrary/MainActivity.java
+++ app/src/main/java/com/emericoapp/demolibrary/MainActivity.java
@@ -1,14 +1,25 @@
 package com.emericoapp.demolibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.emericoapp.mylibrary.Sample;
 
 public class MainActivity extends AppCompatActivity {
 
+    Sample sample = new Sample();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.framlayout,sample);
+        ft.commit();
+
     }
 }
